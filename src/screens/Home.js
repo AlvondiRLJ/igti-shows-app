1f627e3cb6699593714be776773bb79f6943513b
alvondirlj <a.junner@gmail.com>
2020-07-31 00:09:59 -0300

    first commit until implements NavigationContainer

---
+++ src/screens/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+
+const HomeScreen = () => {
+    return (
+        <View style={styles.viewStyle}>
+            <Text>Hello world Alvondi!!</Text>
+        </View>
+    )
+}
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+    viewStyle:{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    }
+})
